900a8c8206269b181758bc498576c88d8c83e863
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-31 17:21:09 -0400

    Initial extraction

---
+++ ohm-vendor.mk
@@ -0,0 +1,45 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/amlogic/ohm
+
+PRODUCT_COPY_FILES += \
+    vendor/amlogic/ohm/proprietary/vendor/etc/bluetooth/qca6174/ar3k/nvm_tlv_3.2.bin:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/qca6174/ar3k/nvm_tlv_3.2.bin \
+    vendor/amlogic/ohm/proprietary/vendor/etc/bluetooth/qca6174/ar3k/rampatch_tlv_3.2.tlv:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/qca6174/ar3k/rampatch_tlv_3.2.tlv \
+    vendor/amlogic/ohm/proprietary/vendor/etc/bluetooth/qca9377/ar3k/nvm_tlv_tf_1.1.bin:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/qca9377/ar3k/nvm_tlv_tf_1.1.bin \
+    vendor/amlogic/ohm/proprietary/vendor/etc/bluetooth/qca9377/ar3k/rampatch_tlv_tf_1.1.tlv:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/qca9377/ar3k/rampatch_tlv_tf_1.1.tlv \
+    vendor/amlogic/ohm/proprietary/vendor/etc/init/android.hardware.oemlock@1.0-service.droidlogic.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.oemlock@1.0-service.droidlogic.rc \
+    vendor/amlogic/ohm/proprietary/vendor/etc/init/android.hardware.security.keymint-service.amlogic.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.security.keymint-service.amlogic.rc \
+    vendor/amlogic/ohm/proprietary/vendor/etc/mesondisplay.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/mesondisplay.cfg \
+    vendor/amlogic/ohm/proprietary/vendor/etc/permissions/android.hardware.hardware_keystore.amlogic.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.hardware_keystore.amlogic.xml \
+    vendor/amlogic/ohm/proprietary/vendor/lib/firmware/athwlan.bin:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/athwlan.bin \
+    vendor/amlogic/ohm/proprietary/vendor/lib/firmware/aucpu_fw.bin:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/aucpu_fw.bin \
+    vendor/amlogic/ohm/proprietary/vendor/lib/firmware/bdwlan30.bin:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/bdwlan30.bin \
+    vendor/amlogic/ohm/proprietary/vendor/lib/firmware/otp30.bin:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/otp30.bin \
+    vendor/amlogic/ohm/proprietary/vendor/lib/firmware/qwlan30.bin:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/qwlan30.bin \
+    vendor/amlogic/ohm/proprietary/vendor/lib/firmware/utf30.bin:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/utf30.bin \
+    vendor/amlogic/ohm/proprietary/vendor/lib/firmware/video/video_ucode.bin:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/video/video_ucode.bin \
+    vendor/amlogic/ohm/proprietary/vendor/lib/firmware/wlan/cfg.dat:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/wlan/cfg.dat \
+    vendor/amlogic/ohm/proprietary/vendor/lib/firmware/wlan/qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/lib/firmware/wlan/qcom_cfg.ini \
+    vendor/amlogic/ohm/proprietary/vendor/lib/teetz/2088528d-102a-4716-b940-23fd9be04adf.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/2088528d-102a-4716-b940-23fd9be04adf.ta \
+    vendor/amlogic/ohm/proprietary/vendor/lib/teetz/2c1a33c0-44cc-11e5-bc3b-0002a5d5c51b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/2c1a33c0-44cc-11e5-bc3b-0002a5d5c51b.ta \
+    vendor/amlogic/ohm/proprietary/vendor/lib/teetz/526fc4fc-7ee6-4a12-96e3-83da9565bce8.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/526fc4fc-7ee6-4a12-96e3-83da9565bce8.ta \
+    vendor/amlogic/ohm/proprietary/vendor/lib/teetz/6c67ce8f-95c1-4826-b62e-1a857a1ced3d.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/6c67ce8f-95c1-4826-b62e-1a857a1ced3d.ta \
+    vendor/amlogic/ohm/proprietary/vendor/lib/teetz/8efb1e1c-37e5-4326-a5d6-8c33726c7d57.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/8efb1e1c-37e5-4326-a5d6-8c33726c7d57.ta \
+    vendor/amlogic/ohm/proprietary/vendor/lib/teetz/d83c3c4a-9e8d-4e4e-ad30-9d40e137f689.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/d83c3c4a-9e8d-4e4e-ad30-9d40e137f689.ta \
+    vendor/amlogic/ohm/proprietary/vendor/lib/teetz/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/e043cde0-61d0-11e5-9c26-0002a5d5c51b.ta \
+    vendor/amlogic/ohm/proprietary/vendor/lib/teetz/e92a43ab-b4c8-4450-aa12-b1516259613b.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/e92a43ab-b4c8-4450-aa12-b1516259613b.ta \
+    vendor/amlogic/ohm/proprietary/vendor/lib/teetz/ff2a4bea-ef6d-11e6-89cc-d4ae52a7b3b3.ta:$(TARGET_COPY_OUT_VENDOR)/lib/teetz/ff2a4bea-ef6d-11e6-89cc-d4ae52a7b3b3.ta
+
+PRODUCT_PACKAGES += \
+    libamavutils \
+    liboemcrypto \
+    libsecmem \
+    libHwAudio_dcvdec \
+    libHwAudio_dtshd \
+    android.hardware.oemlock@1.0-service.droidlogic.xml \
+    android.hardware.security.keymint-service-no-rkp.amlogic.xml \
+    android.hardware.oemlock@1.0-service.droidlogic \
+    android.hardware.security.keymint-service.amlogic
